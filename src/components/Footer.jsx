b7df6dbf53c73ae95eb0fecc58ea9a52c71a5c2d
Lennin666 <soporte@allnexus.com.co>
2021-02-04 16:24:00 -0500

    iniciando el proyecto con react next app

---
+++ src/components/Footer.jsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const footer = () => <h3>Implementado por: Lennin Ibarra</h3>
+
+export default footer;
---
+++ src/components/SubTitle.jsx
@@ -0,0 +1,6 @@
+import React from 'react'
+
+
+const subTitle = () => <h2>Esto es una prueba de @lias</h2>
+
+export default subTitle;
